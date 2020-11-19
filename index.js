ebc34b0609ade32ce564f3d1fda70fdfe57b878c
dbrisco93 <dbrisco93@gmail.com>
2020-11-19 01:02:00 -0600

    commit tree cd7f7fd37e9ec67bf45a0e80550aa58f62ab7fbf

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"]
+const copyOfDrivers = drivers.slice()
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver() {
+    drivers.pop()
+    drivers
+}
+
+function destructivelyRemoveFirstDriver() {
+    drivers.shift()
+    drivers
+}
+
+function appendDriver(name){
+    copyOfDrivers.push(name)
+    return copyOfDrivers
+}
+
+function prependDriver(name){
+    copyOfDrivers.pop()
+    copyOfDrivers.unshift(name)
+    return copyOfDrivers
+
+}
+
+function removeLastDriver(){
+    copyOfDrivers.pop()
+    copyOfDrivers.shift ()
+    return copyOfDrivers
+
+}
+
+function removeFirstDriver(){
+    copyOfDrivers.push('Garfield')
+    copyOfDrivers.shift('Milo')
+    return copyOfDrivers
+}
